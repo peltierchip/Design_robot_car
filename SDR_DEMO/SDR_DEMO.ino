ea1f40467d2e1dfb5391c4858a4949247186f5ed
Thomas Teng <victomteng1997@gmail.com>
2017-03-29 15:19:13 +0800

    SDR_DEMO updated
    Some modifications

--- SDR_DEMO/SDR_DEMO.ino
+++ SDR_DEMO/SDR_DEMO.ino
@@ -10,21 +10,16 @@ const int echoPin1 = 4;
 const int trigPin2 = 5;
 const int echoPin2 = 6;
 long duration, cm1, cm2;
-char mode = 'a';          //mode a: straight line following     b: self control 
+char mode = 'b';          //mode a: straight line following     b: self control 
 int motor_speed;
+char left_direct;
+char right_direct;
 char direct;
 
 //////////////////////////////////////////////////////////////
 
 
-void setup() {
-  // put your setup code here, to run once:
-  Genotronex.begin(9600);
-  Genotronex.println("Start listening from bluetooth");
-  Serial.begin(19200);
-  Serial.println("Start checking infos");
 
-}
 void ultra_reading(){
   
   pinMode(trigPin1, OUTPUT);
@@ -83,7 +78,25 @@ void Motor(char motor, char direct, int spd=0){
       digitalWrite(9, HIGH);
     }}
   }
+  
+void setup() {
+  // put your setup code here, to run once:
+  Genotronex.begin(9600);
+  Genotronex.println("Start listening from bluetooth");
+  Serial.begin(19200);
+  Serial.println("Start checking infos");
+    //Channel A
+  pinMode(12, OUTPUT);
+  pinMode(9, OUTPUT);
+  
+  //Channel B
+  pinMode(13,OUTPUT);
+  pinMode(8,OUTPUT);
 
+  Motor('r','s');
+  Motor('l','s');  
+
+}
   
 void loop() {
   if (Genotronex.available()){
@@ -99,54 +112,65 @@ void loop() {
   }
   }
   if (mode=='a'){
-    Serial.println("mode a working");
+    
     ultra_reading();
+
     int distance = (cm1+cm2)/2;
     if (distance>40){
-      motor_speed = 5*(distance-30)+10;
-      direct = "f";
+      
+      motor_speed = 8*(distance-40)+50;
+      motor_speed = min(motor_speed,255);
+      Serial.println(motor_speed);
+      Motor('r','b',motor_speed);
+      Motor('l','f',motor_speed);
+      
     }
-    if (distance<35){
-      motor_speed = 10*(35-distance)+10;
-      direct = "b";
+    if (distance<30){
+      motor_speed = 5*(30-distance)+50;
+      char left_direct = 'b';
+      char right_direct = 'f';
+      motor_speed = min(motor_speed,255);
+      Motor('r','f',motor_speed);
+      Motor('l','b',motor_speed);
+      Serial.println('b');
+      Serial.println(motor_speed);
     }
     else{
-      direct = "s";
+      char left_direct = 's';
+      char right_direct = 's';
     }
-    Motor("r",direct,motor_speed);
-    Motor("l",direct,motor_speed);
+    
+    
   }
   else if (mode =='b'){
     // control mode here
     Serial.println(String(BluetoothData));
    if(String(BluetoothData)=="f"){   //forward
     Genotronex.println("forward");
-    Motor("r","f",100);
-    Motor("l","f",100);
-    delay(50);
-    BluetoothData = "w";  //change to waiting mode;
+    Motor('r','b',200);
+    Motor('l','f',200);
    }
   if(String(BluetoothData)=="h"){   //backward
     Genotronex.println("backward");
-    Motor("r","b",100);
-    Motor("l","b",100);
-    delay(50);
-    BluetoothData = "w";
+    Motor('r','b',200);
+    Motor('l','f',200);
    }
    if(String(BluetoothData)=="l"){   //left
     Genotronex.println("left");
-    Motor("r","f",50);
-    Motor("l","b",50);
-    delay(50);
-    BluetoothData = "w";
+    Motor('r','f',150);
+    Motor('l','f',150);
+
    }
    if(String(BluetoothData)=="r"){   //right
     Genotronex.println("right");
-    Motor("r","b",50);
-    Motor("l","f",50);
-    delay(50);
-    BluetoothData = "w";
+    Motor('r','b',150);
+    Motor('l','b',150);
     }
+
+    if(String(BluetoothData)=="s"){   //right
+    Genotronex.println("stop");
+    Motor('r','s');
+    Motor('l','s');
     }
    
   }
